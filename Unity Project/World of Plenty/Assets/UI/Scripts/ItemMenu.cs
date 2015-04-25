1d311bf6aa75687d7322226104fc8be2ddf17131
Marcus Ihde <marcusplate@googlemail.com>
2015-04-26 00:34:03 +0200

    Change buy/sell menu.

--- Unity Project/World of Plenty/Assets/UI/Scripts/ItemMenu.cs
+++ Unity Project/World of Plenty/Assets/UI/Scripts/ItemMenu.cs
@@ -12,18 +12,15 @@ public class ItemMenu : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		//AddItem("brick", "Bricks", 0, 0, 0, "brick_img");
-		//AddItem("drugs", "Drugs", 0, 0, 0, "drugs_img");
-		AddItem("Anti-Rad Meds", "Stims", 0, 0, 0, "icon_antiradiation");
-		AddItem("Geiger Counter", "Stims", 0, 0, 0, "icon_geiger");
-		AddItem("Bush Knife", "Stims", 0, 0, 0, "icon_bushknife");
-		AddItem("Pickaxe", "Stims", 0, 0, 0, "icon_pickaxe");
-		AddItem("Fuel Can", "Stims", 0, 0, 0, "icon_fuelcan");
-		AddItem("Bricks", "Stims", 0, 0, 0, "icon_bricks");
+		AddItem("brick", "Bricks", 99, 7, 9, "brick_img");
+		AddItem("drugs", "Drugs", 7, 120, 140, "drugs_img");
+		AddItem("oil", "Oil", 900, 11, 14, "drugs_img");
+		AddItem("waste", "Waste", 999, 3, 4, "drugs_img");
+		AddItem("dirt", "Dirt", 99999, 1, 2, "drugs_img");
 
-		//SetAmount("drugs", 99);
-		//SetSellPrice("drugs", 3);
-		//SetBuyPrice("drugs", 5);
+		SetAmount("drugs", 99);
+		SetSellPrice("drugs", 3);
+		SetBuyPrice("drugs", 5);
 
 		//ClearItems ();
 
@@ -37,10 +34,23 @@ public class ItemMenu : MonoBehaviour {
 	}
 
 	public void AddItem(string id, string name = "None", int amount = 0, int sell = 0, int buy = 0, string image = "none"){
-		var go = GameObject.Instantiate(TextElement);
+		GameObject go = GameObject.Instantiate(TextElement);
+		ItemSingle itm = go.GetComponent<ItemSingle> ();
 
 		go.transform.SetParent(Items.transform);
 		go.name = id;
+		SetItem(id, name, amount, sell, buy, image);
+
+		BindButton (id, itm.sell1, "sell1");
+		BindButton (id, itm.sell10, "sell10");
+		BindButton (id, itm.sell100, "sell100");
+
+		BindButton (id, itm.buy1, "buy1");
+		BindButton (id, itm.buy10, "buy10");
+		BindButton (id, itm.buy100, "buy100");
+	}
+
+	public void SetItem(string id, string name, int amount, int sell, int buy, string image){
 		SetName (id, name);
 		SetImage (id, image);
 		SetAmount (id, amount);
@@ -77,5 +87,11 @@ public class ItemMenu : MonoBehaviour {
 		for (int i = 0; i < Items.transform.childCount; i++) {
 			Destroy(Items.transform.GetChild(i).gameObject);
 		}
+	}
+
+	public void BindButton(string id, Button btn, string action){
+		btn.onClick.AddListener (delegate {
+			Debug.Log(id + ": " + action);
+		});
 	}
 }
--- Unity Project/World of Plenty/Assets/UI/Scripts/ItemSingle.cs
+++ Unity Project/World of Plenty/Assets/UI/Scripts/ItemSingle.cs
@@ -9,6 +9,12 @@ public class ItemSingle : MonoBehaviour {
 	public Text amount;
 	public Text sell;
 	public Text buy;
+	public Button sell1;
+	public Button sell10;
+	public Button sell100;
+	public Button buy1;
+	public Button buy10;
+	public Button buy100;
 
 	// Use this for initialization
 	void Start () {
