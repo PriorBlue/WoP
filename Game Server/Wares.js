3cec46ce2265c4f9cd0f27fe50ac824747747535
AndreiBrasso <andrei.brasso@gmail.com>
2015-04-26 11:19:58 +0200

    Game server and Websocket communication

---
+++ Game Server/Wares.js
@@ -0,0 +1,36 @@
+function Wares() {
+
+	this.waresList = [
+		{ name: 'clay', price: 20 },
+		{ name: 'brick', price: 20 },
+		{ name: 'trash', price: 20 },
+		{ name: 'crap metal', price: 20 },
+		{ name: 'metal', price: 20 },
+		{ name: 'wood', price: 20 },
+		{ name: 'sawn-off shotgun', price: 20 },
+		{ name: 'bush knife', price: 20 },
+		{ name: 'pick axe', price: 20 },
+		{ name: 'empty fuel can', price: 20 },
+		{ name: 'yellow slime', price: 20 },
+		{ name: 'tin can', price: 20 },
+		{ name: 'atomic cola', price: 20 },
+		{ name: 'broken cell phone', price: 20 },
+		{ name: 'copper', price: 20 },
+		{ name: 'wire', price: 20 },
+		{ name: 'electronic parts', price: 20 },
+		{ name: 'radio', price: 20 },
+		{ name: 'geiger counter', price: 20 },
+		{ name: 'flax', price: 20 },
+		{ name: 'textiles', price: 20 },
+		{ name: 'tent', price: 20 },
+		{ name: 'sneakers', price: 20 },
+		{ name: 'anti radiation medication', price: 20 }
+	];
+
+};
+
+
+
+
+module.exports(Wares);
+
---
+++ Unity Project/World of Plenty/Assets/Scripts/NetworkScript.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using SimpleJSON;
+using AssemblyCSharp;
+
+public class NetworkScript : MonoBehaviour {
+
+	// Use this for initialization
+	IEnumerator Start () {
+
+		WaresList waresList = new WaresList ();
+
+		Debug.Log ("TTzpe>  "+waresList.List.ToString ());
+
+		
+		WebSocket w = new WebSocket (new Uri ("ws://localhost:3000"));
+		
+		yield return StartCoroutine(w.Connect());
+		
+		w.SendString(waresList.List.ToString ());
+		int i=0;
+		
+		while (true)
+		{
+			string reply = w.RecvString();
+			
+			if (reply != null)
+			{
+				
+				Debug.Log ("Received: ");
+				var receivedObject = JSON.Parse(reply);
+				Debug.Log(receivedObject["item"]["name"]);
+				w.SendString("Hi there"+i++);
+
+				switch(receivedObject["event"]) {
+
+					case "connected": 
+
+
+						break;
+				}
+
+			}
+			if (w.Error != null)
+			{
+				Debug.LogError ("Error: "+w.Error);
+				break;
+			}
+			yield return 0;
+		}
+		
+		w.Close();
+		
+	}
+}
